51b8c517bb2ccb9c5777d024cb21bc64884f4d3d
Danylo Halaiko <dan.galaiko@gmail.com>
2019-10-31 18:30:43 +0200

    Teacher said me, what to correct.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 int main() {
     cout << "Введіть номер числа Фібоначі: ";
-    unsigned int number, f0 = 0, f1 = 1; //Створюємо змінні для членів послідовності, і номера, який зажадає дізнатися користувач.
+    unsigned int number;
+    unsigned int f0 = 0;//Член послідовності n
+    unsigned int f1 = 1; //Член послідовності n+1
+
     cin >> number;
 
     for (unsigned int i = 2; i <= number; i++) {
-        unsigned long int temp = f0;//Змінна для усунення втрати числа
+        unsigned int temp = f0;//Змінна для усунення втрати числа
         f0 = f1;
         f1 += temp;
     }
